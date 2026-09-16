8672c1dfd55694cb7ef7d455b63a37d7e5c4be9c
Sushan Kannah D <sushan.kd17@gmail.com>
2026-09-16 18:33:42 +0530

    Initialized

---
+++ backend/src/test/java/com/agenticai/questionrepo/QuestionrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agenticai.questionrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionrepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
